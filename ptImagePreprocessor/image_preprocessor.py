13ae7cb8996ac60fbac8259c986830cafa3a529f
Doug Anson <doug.anson@arm.com>
2021-11-09 14:50:59 -0600

    updates for preprocessor

--- ptImagePreprocessor/image_preprocessor.py
+++ ptImagePreprocessor/image_preprocessor.py
@@ -18,103 +18,136 @@
 # ----------------------------------------------------------------------------
 #
 
-import asyncio
-from contextlib2 import AsyncExitStack, asynccontextmanager
-from random import randrange
-from asyncio_mqtt import Client, MqttError
+# General OS
+import sys
+import signal
+import json
+import argparse
 
-# Core imports for the notebook
+# Logging
+import logging
+
+# Asyncio support
+import asyncio
+
+# Paho MQTT
+import paho.mqtt.client as mqtt
+
+# Tensorflow
 import numpy as np 
 import tensorflow as tf
-import time
-import os
-import json
+from tensorflow.keras.preprocessing.image import load_img, img_to_array
 
-# ResNet50 helpers used specifically with our selected pre-trained model
-from tensorflow.keras.applications.resnet50 import preprocess_input
+# Service Name
+SERVICE_NAME = 'ImagePreprocessorService'
 
-loop = asyncio.get_event_loop()
+# MQTT Topics
+RAW_INPUT_TOPIC         = "rawinput"
+PROCESSED_OUTPUT_TOPIC  = "processed"
 
-async def run_image_preprocessor():
-    async with AsyncExitStack() as stack:
-        # Keep track of the asyncio tasks that we create, so that
-        # we can cancel them on exit
-        tasks = set()
-        stack.push_async_callback(cancel_tasks, tasks)
+# Create logger
+logger = logging.getLogger(SERVICE_NAME)
 
-        # Connect to the MQTT broker
-        client = Client("127.0.0.1")
-        await stack.enter_async_context(client)
+# Signal handler to exit the service...
+def exit_signal(signum, frame):
+    logger.info(SERVICE_NAME + ": Closing down on signal: {signum}".format(signum=signum))
+    sys.exit(0)
 
-        # You can create any number of topic filters
-        topic_filters = (
-            "rawinput",
-        )
-        for topic_filter in topic_filters:
-            # Log all messages that matches the filter
-            manager = client.filtered_messages(topic_filter)
-            messages = await stack.enter_async_context(manager)
-            template = f'topic:{topic_filter}, payload:{{}}'
-            task = loop.create_task(log_messages(messages, template))
-            tasks.add(task)
-
-        # Messages that doesn't match a filter will get logged here
-        messages = await stack.enter_async_context(client.unfiltered_messages())
-        task = loop.create_task(log_messages(messages, "[unfiltered] {}"))
-        tasks.add(task)
+# Our Image Preprocessor Service as a Thread
+class ImagePreprocessorService():
+    # Constructor
+    def __init__(self):
+        self.name = SERVICE_NAME
+        self.client = mqtt.Client()
+        self.client.on_connect = self.on_connect
+        self.client.on_message = self.on_message
+        self.client.on_subscribe = self.on_subscribe
         
-        # Subscribe to topic(s)
-        # 🤔 Note that we subscribe *after* starting the message
-        # loggers. Otherwise, we may miss retained messages.
-        await client.subscribe("rawinput")
+    # OnSubscribe
+    def on_subscribe(self, obj, mid, granted_qos):
+        logger.info("Subscribed to: " + str(mid) + " " + str(granted_qos))
+        
+    # OnConnect
+    def on_connect(self, client, userdata, flags, rc):
+        logger.info("Connected with result code "+str(rc) + ". Subscribing to: " + RAW_INPUT_TOPIC + "...")
+        client.subscribe(RAW_INPUT_TOPIC)
+        
+    # OnMessage
+    def on_message(self, client, userdata, msg):
+        # Convert the payload to JSON...
+        msg_json_str = msg.payload.decode("utf-8");
+        msg_json = json.loads(msg_json_str)
+        
+        # Debug
+        logger.info("Topic: " + msg.topic + " Payload: " + json.dumps(msg_json))
+        
+        # Process the input command
+        self.process_command(msg_json)
+        
+    # SendMessage
+    def send_message(self, topic, msg_str):
+        infot = self.client.publish(topic, msg_str)
+        infot.wait_for_publish()
+            
+    # Keras50 preprocessing
+    def keras50_preprocess(self, json):
+        logger.info("Keras50 Preprocessing: " + json.dumps(json))
+        self.send_message(PROCESSED_OUTPUT_TOPIC,json['timestamp'])
+        
+    # Raw preprocessing
+    def raw_preprocess(self, json):
+        logger.info("Raw Preprocessing: " + json.dumps(json))
+        
+    # Process the input command
+    def process_command(self, json):
+        model = json['model']
+        if json['model'] == 'keras50':
+            # preprocessing images for keras50... 
+            self.keras50_preprocess(json)
+        else:
+            # non-preprocessing - just passing raw input...
+            logger.info("WARNING: Model " + model + " has no registered preprocessor function... input files not changed (OK)")
+            self.raw_preprocess(json)
+        
+    # Service main loop
+    async def main_loop(self,name,loop,mqtt_hostname,mqtt_port):
+        self.mloop = loop
+        self.name = name
+        
+        # Connect to MQTT Broker
+        self.client.connect(mqtt_hostname, mqtt_port, 60)
+        
+        # Enter processing loop
+        self.client.loop_forever()
+        
+# Main 
+async def main(mLoop):
+    # Initialize signals
+    signal.signal(signal.SIGTERM, exit_signal)
+    
+    # Initialize logging level
+    logging.basicConfig(level=logging.INFO, filemode='w', format='[%(levelname)s] %(name)s - %(message)s')
+    
+    # Parse any arguments we have passed
+    parser = argparse.ArgumentParser(
+        description='Pelion Image Preprocessor Service',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-n', '--name', dest="name", help='service name', type=str, default=SERVICE_NAME)
+    args = parser.parse_args()
+    
+    # Create the service
+    service = ImagePreprocessorService()
+    logger.info("Pelion Image Preprocessor Service: Starting...")
+    
+    # Run the service
+    await service.main_loop(args.name,mLoop,"127.0.0.1",1883)
+    
+    # Exited the service loop
+    logger.error("Service's main loop has exited. Exiting...");
+    exit(1)
 
-        # Publish a random value to each of these topics
-        #topics = (
-        #    "predict",
-        #)
-        #task = loop.create_task(post_to_topics(client, topics))
-        #tasks.add(task)
-
-        # Wait for everything to complete (or fail due to, e.g., network
-        # errors)
-        await asyncio.gather(*tasks)
-
-async def post_to_topics(client, topics):
-    while True:
-        for topic in topics:
-            message = randrange(100)
-            print(f'topic={topic} message={message}')
-            await client.publish(topic, message, qos=1)
-            await asyncio.sleep(2)
-
-async def log_messages(messages, template):
-    async for message in messages:
-        # 🤔 Note that we assume that the message paylod is an
-        # UTF8-encoded string (hence the `bytes.decode` call).
-        print(template.format(message.payload.decode()))
-
-async def cancel_tasks(tasks):
-    for task in tasks:
-        if task.done():
-            continue
-        task.cancel()
-        try:
-            await task
-        except asyncio.CancelledError:
-            pass
-
-async def main():
-    # Run the advanced_example indefinitely. Reconnect automatically
-    # if the connection is lost.
-    reconnect_interval = 3  # [seconds]
-    while True:
-        try:
-            await run_image_preprocessor()
-        except MqttError as error:
-            print(f'Error "{error}". Reconnecting in {reconnect_interval} seconds.')
-        finally:
-            await asyncio.sleep(reconnect_interval)
-
-
-futures = [main()]
-loop.run_until_complete(asyncio.wait(futures))
+# Main Call
+if __name__ == "__main__":
+    mLoop = asyncio.get_event_loop()
+    mLoop.run_until_complete(main(mLoop))
+    mLoop.close()
